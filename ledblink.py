6f4f8ef7a59c3984a4ed584865074ff13c50d8c8
timka1124 <timka1124@gmail.com>
2019-09-26 02:47:10 +0100

    led

---
+++ ledblink.py
@@ -0,0 +1,13 @@
+﻿import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
+
+while True:
+    GPIO.output(14, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(14,GPIO.OUT)
+    time.sleep(1)
+
+GPIO.cleanup()
